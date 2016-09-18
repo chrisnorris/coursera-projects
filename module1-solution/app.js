a05ec698e4fcf3654d8e12775a1484957a071254
chrisnorris <cnorris2007@gmail.com>
2016-09-18 15:55:35 +0100

    implemented BONUS (OPTIONAL AND NOT GRADED)

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,14 +1,29 @@
 (function() {
     'use strict';
+
     angular.module('LunchCheck', [])
         .controller('LunchCheckController', function($scope) {
 
+            $scope.cssColor = "red";
             $scope.menuItems = "";
             $scope.message = "";
+            $scope.setCss = function() {
+                if ($scope.menuItems != "") {
+                    $scope.cssColor = "green";
+                } else {
+                    $scope.cssColor = "red";
+                }
+            }
             $scope.checkMeal = function() {
                 if ($scope.menuItems != "") {
-                    var items = $scope.menuItems.split(",").length;
-                    if (items <= 3 && items > 0) {
+                    var items = $scope.menuItems.split(",");
+                    var realCount = 0;
+                    items.forEach(function(item, index) {
+                        if (item.trim().length > 0) {
+                            realCount += 1;
+                        };
+                    })
+                    if (realCount <= 3 && realCount > 0) {
                         $scope.message = "Enjoy!"
                     } else {
                         $scope.message = "Too much!";
@@ -19,3 +34,4 @@
             }
         });
 })();
+
