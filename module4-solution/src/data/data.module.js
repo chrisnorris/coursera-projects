a168074421bf98465caec5efb681121d3393ecc7
chris norris <cnorris2007@gmail.com>
2016-10-08 12:47:42 +0100

    Add Data module

--- module4-solution/src/data/data.module.js
+++ module4-solution/src/data/data.module.js
@@ -1,1 +1,6 @@
 //Create data.module.js file and declare another module in it called data. Make sure the MenuApp module lists the data module as a dependency.
+(function(){
+'use strict';
+ angular.module('Data', []);
+ 
+})();
